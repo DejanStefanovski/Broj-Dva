15350a3d042a3a7ab4545c19ac0b3b864ecf17fc
DejanStefanovski <dejanstefanovskister@gmail.com>
2021-03-10 10:51:36 +0100

    Broj Tri

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+K
+src/com/company/clasa1.java,8\6\868eee215d6e4e7c4f828f9b6b600a43501de1db
+I
+src/com/company/Main.java,4\a\4a831b10439099d44774d0ccc387e808deffaeab
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+            System.out.println("HelloWorld!!!");
+            System.out.println("Test");
+            System.out.println("Print");
+            System.out.println("Prijatno");
+
+
+	  }
+}
+ 
